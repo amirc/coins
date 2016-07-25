25b81eb24ef6e264c9868d4691fb9204a5b80549
Amir Lellouche <lalosher@gmail.com>
2016-07-25 17:10:36 +0300

    inita

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 20.0f;
+
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < 0) {
+			transform.position = new Vector3 (0, 0.06f, 0);
+		}
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		print (h + v);
+		Vector3 movment = new Vector3 (h, 0, v);
+		rb.AddForce (movment.normalized * speed);
+	}
+}
---
+++ Assets/enemyControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyControl : MonoBehaviour {
+	public GameObject target;
+	public float speed = 10;
+	public float attackRange = 0;
+	public float aimTime = 0.5f;
+
+	private Rigidbody rb;
+	private float nextAttack;
+
+	private Vector3 nextAttackDirc;
+	private bool aimed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		nextAttack = Time.time + Random.value * attackRange;
+		aimed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!aimed && nextAttack - aimTime < Time.time) {
+			nextAttackDirc = target.transform.position - transform.position;
+			nextAttackDirc.Normalize ();
+			aimed = true;
+		}
+
+		if (nextAttack < Time.time && aimed) {
+			print("Attak!!");
+			rb.AddForce (nextAttackDirc.normalized * speed);
+
+			// Setting next attack:
+			nextAttack = Time.time + Random.value * attackRange + 1;
+			aimed = false;
+		}
+	}
+}
